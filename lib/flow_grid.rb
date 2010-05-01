f6ca6d86128a63e0b5cd356fb3bbce7417a16344
Bill Siggelkow <bsiggelkow@mac.com>
2010-05-01 15:28:05 -0400

    Refactored to use options instead of positional arguments

--- lib/flow_grid.rb
+++ lib/flow_grid.rb
@@ -2,11 +2,25 @@ require 'active_support/core_ext'
 # FlowGrid
 module FlowGridHelper
   # Renders an HTML table by looping over the items and creating a table cell for each item.
-  # The content of the cell is rendered by passing the item to the partial.
+  # The content of the cell can be rendered by passing the item to the partial.
+  # If a block is given, it acts as a transformer for the collection item. If a partial is specified
+  # along with the block, then the transformation will be performed before passing the item to the partial.
   # You can specify the number of columns that the table will have, and if the columns
   # should be laid down horizontally across or vertically down. A &nbsp; will be inserted
   # into any extra cells.
-  def flow_grid(collection, partial, columns=1, orientation=:horizontal, options={}, &block)
+  # Options supported:
+  # * :partial => partial to render (passed to <code>render</code> so do not preface with _)
+  # * :columns => number of columns for the grid (defaults to 1)
+  # * :flow    => dictates if the values will flow horizontal or vertical (defaults to horizontal)
+  # * :fill    => fill grid cell with this if collection item is nil (defaults to '&nbsp;')
+  # Any remaining options will be treated as html options for the generated <code>table</code> tag
+  def flow_grid(collection, options={}, &block)
+
+    partial     = options.delete(:partial)
+    columns     = options.delete(:columns) || 1
+    flow        = options.delete(:flow) || :horizontal
+    fill        = options.delete(:fill) || '&nbsp;'
+    
     table_options = {:class => :flow_grid}.update(options.stringify_keys)
     rows = (collection.length.to_f / columns.to_f).ceil
     i = 0 # item counter
@@ -14,7 +28,7 @@ module FlowGridHelper
     result = tag :table, table_options, true # create the <table> tag
     rows.times do
       j = j + 1
-      i = j if orientation == :vertical
+      i = j if flow == :vertical
       result += "<tr>"
       columns.times do
         result += "<td>"
@@ -22,11 +36,11 @@ module FlowGridHelper
           item = block_given? ? yield( collection[i] ) : collection[i]
           result += partial ? 
             render(:partial => partial, :object => item) :
-            item
+            item.to_s
         else
-          result += "&nbsp;"
+          result += fill
         end       
-        i = (orientation == :horizontal ? i + 1 : i + rows)
+        i = (flow == :horizontal ? i + 1 : i + rows)
         result += "</td>"
       end
       
--- test/flow_grid_test.rb
+++ test/flow_grid_test.rb
@@ -21,21 +21,36 @@ class FlowGridTest <  ActiveSupport::TestCase
     html = %Q{
       <table class="flow_grid"><tr><td>foo</td></tr><tr><td>bar</td></tr><tr><td>baz</td></tr></table>
     }
-    assert_equal html.strip, @helper.flow_grid(@collection, 'item')
+    assert_equal html.strip, @helper.flow_grid(@collection, :partial => 'item')
+  end
+
+  test "should convert the collection item to a string" do
+    collection = [1,2,3]
+    html = %Q{
+      <table class="flow_grid"><tr><td>1</td></tr><tr><td>2</td></tr><tr><td>3</td></tr></table>
+    }
+    assert_equal html.strip, @helper.flow_grid(collection)
+  end
+
+  test "should flow the values vertically" do
+    html = %Q{
+      <table class="flow_grid"><tr><td>foo</td><td>baz</td></tr><tr><td>bar</td><td>&nbsp;</td></tr></table>
+    }
+    assert_equal html.strip, @helper.flow_grid(@collection, :columns => 2, :flow => :vertical )
   end
 
   test "should render the partial for the collection" do
     @collection.each do |item|
       @helper.expects(:render).with({:partial => 'item', :object => item}).returns(item)
     end
-    @helper.flow_grid(@collection, 'item')
+    @helper.flow_grid(@collection, :partial => 'item')
   end
   
   test 'should call the block' do
     html = %Q{
       <table class="flow_grid"><tr><td>oof</td></tr><tr><td>rab</td></tr><tr><td>zab</td></tr></table>
     }
-    res = @helper.flow_grid(@collection, nil) {|t| t.reverse}
+    res = @helper.flow_grid(@collection) {|t| t.reverse}
     assert_equal html.strip, res
   end
   
